b65f677bab8e7c69b9254fdd6eb474b3e013c981
dBuck <dbuck008@gmail.com>
2015-04-30 04:39:52 -0400

    RGB "no openCV" methods

--- app/src/main/java/io/xsor/resistor/MainActivity.java
+++ app/src/main/java/io/xsor/resistor/MainActivity.java
@@ -1,25 +1,21 @@
 package io.xsor.resistor;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
@@ -28,32 +24,27 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.opencv.core.Core.addWeighted;
-import static org.opencv.core.Core.inRange;
 import static org.opencv.core.Core.rectangle;
-import static org.opencv.core.Core.vconcat;
 import static org.opencv.highgui.Highgui.imwrite;
-import static org.opencv.imgproc.Imgproc.COLOR_BGR2HSV;
-import static org.opencv.imgproc.Imgproc.COLOR_BGR2HSV_FULL;
-import static org.opencv.imgproc.Imgproc.COLOR_HSV2RGB;
 import static org.opencv.imgproc.Imgproc.COLOR_RGB2HSV;
-import static org.opencv.imgproc.Imgproc.COLOR_RGB2HSV_FULL;
-import static org.opencv.imgproc.Imgproc.COLOR_RGB2RGBA;
-import static org.opencv.imgproc.Imgproc.COLOR_RGBA2BGR;
 import static org.opencv.imgproc.Imgproc.COLOR_RGBA2RGB;
-import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.GaussianBlur;
 import static org.opencv.imgproc.Imgproc.cvtColor;
-import static org.opencv.imgproc.Imgproc.filter2D;
-import static org.opencv.imgproc.Imgproc.threshold;
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2, View.OnTouchListener {
 
     final static String TAG = "Main Activity";
 
+    private static int FIRST_DERIVATIVE = 1;
+    private static int SECOND_DERIVATIVE = 2;
+
     private CameraView mOpenCvCameraView;
+    private TextView resistorValueText, band1, band2, band3, band4, band5, band6, band7, band8, band9, band10;
+
+
     private int rectangleHeight;
     private int rectangleWidth;
 
@@ -66,6 +57,21 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     Scalar redLBU = new Scalar(RED_MINU, 0, 30);
     Scalar redUBU = new Scalar(RED_MAXU, 255, 255);
 
+    static double colorRGBValues[][] = {
+            { 20, 20, 20 }, // black
+//            { 71, 53, 38 }, // brown
+            { 180, 20, 53 }, // red
+//            { 160, 90, 50 }, // orange
+            { 157, 123, 39 }, // yellow
+            { 41, 90, 46 }, // green
+//            { 40, 73, 86 }, // blue
+            { 75, 55, 75 }, // violet
+//            { 73, 65, 62 }, // gray
+            { 200, 200, 200 } // white
+    };
+    private int WHITE = 5;
+    private int LINE_SIZE = 137;
+    private boolean filter = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,14 +79,26 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
         mOpenCvCameraView = (CameraView) findViewById(R.id.OpenCvCameraView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setOnTouchListener(this);
+        resistorValueText = (TextView) findViewById(R.id.resistorValue);
+        band1 = (TextView) findViewById(R.id.band1);
+        band2 = (TextView) findViewById(R.id.band2);
+        band3= (TextView) findViewById(R.id.band3);
+        band4= (TextView) findViewById(R.id.band4);
+        band5= (TextView) findViewById(R.id.band5);
+        band6= (TextView) findViewById(R.id.band6);
+        band7= (TextView) findViewById(R.id.band7);
+        band8= (TextView) findViewById(R.id.band8);
+        band9= (TextView) findViewById(R.id.band9);
+        band10= (TextView) findViewById(R.id.band10);
+
 
         rectangleHeight = dpToPx(35);
         rectangleWidth = dpToPx(50);
-
     }
 
     @Override
@@ -97,8 +115,8 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
-                    //mOpenCvCameraView.flashOn(false);
-                    //mOpenCvCameraView.setFlashMode(getBaseContext(),4);
+//                    mOpenCvCameraView.flashOn(false);
+//                    mOpenCvCameraView.setFlashMode(getBaseContext(),4);
                 } break;
                 default:
                 {
@@ -122,8 +140,8 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
             mOpenCvCameraView.disableView();
     }
 
-    public void onDestroy() {
-        super.onDestroy();
+    public void onStop() {
+        super.onStop();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
@@ -171,15 +189,14 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
             vValues += " " + HSV[2];
         }
 
+//        Log.d(TAG, "hValues: " + hValues);
+//        Log.d("", "");
+//        Log.d(TAG, "sValues: " + sValues);
+//        Log.d("","");
+//        Log.d(TAG ,"vValues: " + vValues);
 
 
-
-
-        Log.d(TAG, "hValues: " + hValues);
-        Log.d("", "");
-        Log.d(TAG, "sValues: " + sValues);
-        Log.d("","");
-        Log.d(TAG ,"vValues: " + vValues);
+        doMagic(frameROILine);
 
         return frameCopy;
     }
@@ -199,32 +216,226 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
             Log.d(TAG, "SUCCESS Save: " + path + filename);
         else
             Log.d(TAG, "Fail writing image to external storage");
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGBA);
+
     }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 
     public int dpToPx(int dp) {
         DisplayMetrics displayMetrics = getBaseContext().getResources().getDisplayMetrics();
         return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    private double doMagic(final Mat image) {
+//        Mat newImage = new Mat();
+        if(filter){
+            Size size = new Size(1, 5);
+            GaussianBlur(image, image, size, 0);
+        }
+
+        int [] newImage = new int[(int)image.size().width];
+        for(int i = 0; i < image.size().width; i++){
+            newImage[i] = (int) findColor(image.get(0,i));
+        }
+
+        // take derivative and threshold image clearly define edges
+        int[] derivativeImage = imageDerivative(newImage, FIRST_DERIVATIVE);
+
+        // find position of edges as pixel cord.
+        int[] edgePositions = segmentBands(derivativeImage);
+
+        // TODO:
+        // scan edge positions and check changes - remove local changes (within 5 pixels)
+
+        // find average pixel color between edge locations
+        int [] bandValues = new int[edgePositions.length + 1];
+        if(edgePositions.length > 0){
+            int i;
+            for(i = 0; i < edgePositions.length; i++){
+                if(i == 0){
+                    bandValues[i] = averageArray(Arrays.copyOfRange(newImage, 0, edgePositions[0])); // first band
+                }else{
+                    bandValues[i] = averageArray(Arrays.copyOfRange(newImage, edgePositions[i-1], edgePositions[i])); // get all values between to edges
+                }
+            }
+            bandValues[i] = averageArray(Arrays.copyOfRange(newImage, edgePositions[i-1], newImage.length)); // last band
+        }
+        Log.e(TAG, "Band Values: " + Arrays.toString(bandValues));
+
+        int uniqueBands = 1;
+        for(int i = 1; i < bandValues.length; i++){
+            if(bandValues[i-1] != bandValues[i]){
+                if(bandValues[i] != WHITE){
+                    uniqueBands++;
+                }
+            }
+        }
+
+        int lastInputValue = 255;
+        int input = 0;
+        final int[] resBands = new int[uniqueBands+1];
+        resBands[0] = bandValues[0];
+        for(int i = 1; i < bandValues.length; i++){
+            if(bandValues[i] != WHITE){
+                if(bandValues[i] != lastInputValue || bandValues[i - 1] == WHITE){
+                    lastInputValue = bandValues[i];
+                    resBands[++input] = bandValues[i];
+                }
+            }
+        }
+
+        Log.e(TAG, "Unique Bands: " + Arrays.toString(resBands));
+
+        // calc value
+        final double resistorValue = calcResValue(resBands);
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                //* The Complete ProgressBar does not appear**/
+                resistorValueText.setText("" + resistorValue);
+            }
+        });
+
+        Log.e(TAG, "RESISTOR VALUE: " + resistorValue);
+        Log.e(TAG, "***********************************");
+
+        return resistorValue;
+    }
+
+    /* Find Color
+     * @params:
+     *	rgb_value: rgb array of a single pixel
+     * @return:
+     *	colorHyp: closest color found to that pixel from those defined above
+     */
+    public double findColor(final double rgb_value[]){
+//        Log.i(TAG, "RGB: " + Arrays.toString(rgb_value));
+        double colorHyp = 0;
+        double minDist = 255;
+        double dist = 0;
+
+        for(int i = 0; i < colorRGBValues.length; i++){
+            // find euclidian distance between RGB of current pixel with predefined colors
+            // sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
+            dist = Math.sqrt(Math.pow((rgb_value[0]-colorRGBValues[i][0]),2)+Math.pow((rgb_value[1]-colorRGBValues[i][1]),2)+Math.pow((rgb_value[2]-colorRGBValues[i][2]),2));
+//            Log.e(TAG, "dist: " + dist);
+
+            if(dist < minDist){ // if this distance is smaller than the rest, this reg_value is more likely this new color
+                minDist = dist;
+//                Log.i(TAG, "minDist: " + minDist);
+                colorHyp = i;
+            }
+        }
+
+//        Log.e(TAG, "Color: " + colorCodeName[(int)colorHyp]);
+        return colorHyp;
+    }
+
+    /* Calculate Resistor Value
+     * @params:
+     *	rings: Array of ints that correspond to the colors of the resistor bands as defined above
+     * @return:
+     *	result: int value of resistor
+     */
+    static double calcResValue(final int rings[]) {
+        if(rings.length < 2) return 0;
+        double result = 0;
+
+        for(int i = 0; i < rings.length - 2; i++){
+            result += rings[i] * Math.pow(10,(rings.length - 3 - i));
+        }
+        result *= Math.pow(10, rings[rings.length - 2]);
+
+        return result;
+    }
+
+    /*
+     * @params:
+     *	image: 1D image
+     *	method:
+     *		- 1st derivative
+     *		- 1nd derivative
+     * @return:
+     *	averageImage: Mat of calculated derivative values for each pixel using method @param{method}
+     */
+    public int[] imageDerivative(final int[] image, int method){
+        int[] imageDerivative = new int[LINE_SIZE];
+
+        // scan every pixel and determine change in gradient using 1st derivative
+        // ignore first and last pixel
+        for(int i = 1; i < image.length-1; i++){
+            if(method == FIRST_DERIVATIVE){
+                imageDerivative[i] = (image[i+1] - image[i]);
+//                imageDerivative.put(0, i, (image.get(0,i+1)[0] - image.get(0,i)[0]));
+            }else if(method == SECOND_DERIVATIVE){
+                imageDerivative[i] = (image[i+1] + image[i-1] - 2 * image[i]);
+//                imageDerivative.put(0, i, (image.get(0,i+1)[0] + image.get(0,i-1)[0] - 2 * image.get(0,i)[0]));
+            }
+        }
+        return imageDerivative;
+    }
+
+//    public Mat imageDerivative(final Mat image){
+//        return imageDerivative(image, FIRST_DERIVATIVE);
+//    }
+
+
+    /* Threshold
+     * @params:
+     *	image: 1D Mat image
+     *	threshold: threshold value
+     * @return:
+     *	result: binary image
+     */
+    public int[] threshold(final int[] image, int threshold){
+        int[] result = image;
+
+        for(int i = 0; i < image.length; i++){
+            if(image[i] < threshold){
+                result[i] = 0;
+            }else{
+                result[i] = 255;
+            }
+        }
+        return result;
+    }
+
+    /* Segment Bands
+     * @params:
+     *	binary image
+     * @return:
+     *	array of edge locations as pixel value
+     */
+    public int[] segmentBands(final int[] image){
+//        int[] threshold = threshold(image, 10); // play around with this number until we like the results
+        int[] bandEdges = new int[LINE_SIZE]; // must change if planing on scaling
+        int edgeCount = 0;
+
+        // find location of edges
+        for(int i = 0; i < image.length; i++){
+            if(Math.abs(image[i]) > 0){ // if edge is detected
+                bandEdges[edgeCount] = i; // keep track of position in original image
+                edgeCount++;
+            }
+        }
+
+        return Arrays.copyOfRange(bandEdges, 0, edgeCount);
+    }
+
+    /* Average Array
+     * @parmams:
+     *	array of pixel values (ints)
+     * @return:
+     *	average pixel value
+     */
+    public int averageArray(final int[] array){
+        double average = 0;
+
+        for (int anArray : array) {
+            average += anArray;
+        }
+        average /= (double) array.length;
+
+        return (int) Math.round(average);
     }
 }
